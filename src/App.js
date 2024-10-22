773cc97dd6fe034879d8167c8fbaeea33787b24a
rushang9 <59726412+rushang9@users.noreply.github.com>
2024-10-22 22:09:16 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Hero/Hero';
+import Programs from './components/Programs/Programs';
+import Title from './components/Title/Title';
+import About from './components/About/About';
+import Campus from './components/Campus/Campus';
+import Testimonial from './components/Testimonial/Testimonial';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
+import Videoplayer from './components/Videoplayer/Videoplayer';
 
-function App() {
+const App = () => {
+
+  const [playState,setPlaystate] = useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Hero />
+      <div className='container'>
+        <Title subtitle = 'our program' title = 'What We Offer' />
+      <Programs />
+      <About setPlaystate = {setPlaystate}/>
+      <Title subtitle = 'Gallery' title = 'Campus Photos' />
+      <Campus />
+      <Title subtitle = 'TESTIMONIALS' title = 'What Student Says' />
+      <Testimonial />
+      <Title subtitle = 'Contact us' title = 'get in touch' />
+      <Contact />
+      <Footer />
+      </div>
+      <Videoplayer playState = {playState}  setPlaystate = {setPlaystate} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
